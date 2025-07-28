bc29e64a663f1aaeed0e8c008f12234edadf9d9d
song <sooong137@gmail.com>
2025-07-28 17:08:47 +0900

    rest template, docker-compose

--- src/main/java/com/example/ordersystem/ordering/service/OrderingService.java
+++ src/main/java/com/example/ordersystem/ordering/service/OrderingService.java
@@ -2,22 +2,53 @@ package com.example.ordersystem.ordering.service;
 
 import com.example.ordersystem.ordering.domain.Ordering;
 import com.example.ordersystem.ordering.dto.OrderCreateDto;
+import com.example.ordersystem.ordering.dto.ProductDto;
+import com.example.ordersystem.ordering.dto.ProductUpdateStockDto;
 import com.example.ordersystem.ordering.repository.OrderingRepository;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 
 @Service
+@RequiredArgsConstructor
 @Transactional
 public class OrderingService {
     private final OrderingRepository orderingRepository;
-
-    public OrderingService(OrderingRepository orderingRepository) {
-        this.orderingRepository = orderingRepository;
-    }
+    private final RestTemplate restTemplate;
 
     public Ordering orderCreate(OrderCreateDto orderDto, String userId){
+
+        String productGetUrl = "http://product-service/procuct/" + orderDto.getProductId();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("X-User-Id", userId);
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+
+        ResponseEntity<ProductDto> response = restTemplate.exchange(productGetUrl, HttpMethod.GET, httpEntity, ProductDto.class);
+        ProductDto productDto = response.getBody();
+
+        int quantity = orderDto.getProductCount();
+
+        if (productDto.getStockQuantity() < quantity) {
+            throw new IllegalArgumentException("재고 부족");
+        } else {
+            String productPutUrl = "http://product-service/product/updatestock";
+
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<ProductUpdateStockDto> updateEntity = new HttpEntity<>(
+                    ProductUpdateStockDto.builder()
+                            .productId(orderDto.getProductId())
+                            .productQuantity(orderDto.getProductCount())
+                            .build(), httpHeaders
+            );
+
+            restTemplate.exchange(productPutUrl, HttpMethod.PUT, updateEntity, Void.class);
+        }
 
         Ordering ordering = Ordering.builder()
                 .memberId(Long.parseLong(userId))
